cb3142b3bd193397c61a3fd49821714f088252b1
NikaChapo <nikachapidze01>
2021-01-13 11:37:58 +0400

    Add dependencies

---
+++ app/src/androidTest/java/com/epam/newsapp/ui/login/LoginActivityTest.kt
@@ -0,0 +1,15 @@
+package com.epam.newsapp.ui.login
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+@MediumTest
+class LoginActivityTest {
+
+}
